d0ff2238ce0fafb7d4ea1377894bfe727786434d
Utilisateur home <nomail>
2013-08-06 23:15:23 +0200

    Add principal fragment_1 Add TabPager in  MainActivity Change Contenu text value of the fragment with the tab text value

--- DoWellMarketSketch/gen/com/example/dowellmarketsketch/R.java
+++ DoWellMarketSketch/gen/com/example/dowellmarketsketch/R.java
@@ -952,9 +952,10 @@ containing a value of this type.
         public static final int abs__textButton=0x7f040013;
         public static final int abs__title=0x7f04001b;
         public static final int abs__up=0x7f04000b;
-        public static final int action_settings=0x7f040032;
+        public static final int action_settings=0x7f040033;
         public static final int disableHome=0x7f040009;
         public static final int edit_query=0x7f040026;
+        public static final int fragment_txt=0x7f040032;
         public static final int homeAsUp=0x7f040006;
         public static final int listMode=0x7f040002;
         public static final int normal=0x7f040001;
@@ -991,8 +992,9 @@ containing a value of this type.
         public static final int abs__search_view=0x7f030013;
         public static final int abs__simple_dropdown_hint=0x7f030014;
         public static final int activity_main=0x7f030015;
-        public static final int sherlock_spinner_dropdown_item=0x7f030016;
-        public static final int sherlock_spinner_item=0x7f030017;
+        public static final int fragment_1=0x7f030016;
+        public static final int sherlock_spinner_dropdown_item=0x7f030017;
+        public static final int sherlock_spinner_item=0x7f030018;
     }
     public static final class menu {
         public static final int main=0x7f0b0000;
--- DoWellMarketSketch/src/com/example/dowellmarketsketch/MainActivity.java
+++ DoWellMarketSketch/src/com/example/dowellmarketsketch/MainActivity.java
@@ -1,14 +1,130 @@
 package com.example.dowellmarketsketch;
 
-import com.actionbarsherlock.app.SherlockFragmentActivity;
+import java.util.ArrayList;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 
 public class MainActivity extends SherlockFragmentActivity {
-
+	private ViewPager mViewPager;
+	private ActionBar mSupportActionBar;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		//setContentView(R.layout.activity_main);
+		
+		init();
 	}
+	
+	 /**
+     * Initialises the Views and variables for this activity.
+     */
+    private void init() {
+        
+        //  Initialise and set viewpager to the activity.
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(1232);
+        setContentView(mViewPager);
+        
+        //  Init and set ActionBar Properties.
+        mSupportActionBar = getSupportActionBar();
+        mSupportActionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        
+        //  Initialise Adapter for the view pager.
+        SherlockTabAdapter adapter = new SherlockTabAdapter();
+        ArrayList<Bundle> bundleAL = new ArrayList<Bundle>();
+        String texts[] = {"Maison", "Appartement", "Porte-feuille", "Autres"};
+        for(int index = 0; index < texts.length; index++)
+        {
+            //  Prepare Bundle object for each tab.
+            Bundle bundle = new Bundle();
+            bundle.putString("text", texts[index]);
+            bundleAL.add(bundle);
+            
+            //  Add tabs for the action bar.
+            ActionBar.Tab tab = mSupportActionBar.newTab().setText(texts[index])
+                    .setTabListener(adapter);
+            mSupportActionBar.addTab(tab);
+        }
+        
+        adapter.setBundle(bundleAL);
+        mViewPager.setAdapter(adapter);
+        mViewPager.setOnPageChangeListener(adapter);
+    }
+    
+    /**
+     * Adapter for setting the content for the tab click's.
+     * 
+     * @author shpolavarapu
+     * 
+     */
+    private class SherlockTabAdapter extends FragmentPagerAdapter implements ActionBar.TabListener,
+            ViewPager.OnPageChangeListener {
+
+        private ArrayList<Bundle> mBundleAL;
+        
+        public SherlockTabAdapter() {
+            super(getSupportFragmentManager());
+        }
+        
+        /**
+         * ArrayList of bundle's to pass as Arguments to Fragment.
+         * @param bundleAL  ArrayList of bundle's
+         */
+        void setBundle(ArrayList<Bundle> bundleAL) {
+            mBundleAL = bundleAL;
+        }
+        @Override
+        public void onPageScrollStateChanged(int position) {
+            
+        }
+
+        @Override
+        public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            //  Change the tab selection upon page selection.
+            mSupportActionBar.setSelectedNavigationItem(position);
+        }
+
+        @Override
+        public void onTabReselected(Tab arg0, FragmentTransaction arg1) {
+
+        }
+
+        @Override
+        public void onTabSelected(Tab tab, FragmentTransaction arg1) {
+            //  On Tab selection change the View Pager's Current item position. 
+            mViewPager.setCurrentItem(tab.getPosition());
+        }
+
+        @Override
+        public void onTabUnselected(Tab arg0, FragmentTransaction arg1) {
+
+        }
+
+        @Override
+        public Fragment getItem(int pos) {
+            return Fragment.instantiate(MainActivity.this, Fragment_1.class.getName(), mBundleAL.get(pos));
+        }
+
+        @Override
+        public int getCount() {
+            return mBundleAL.size();
+        }
+
+    }
+
 
 }
