d9b4e0b486f165831db4cdfc0e5c601281da40c3
cirenege <cirenege@LA81835.chu-lyon.fr>
2013-08-22 17:48:06 +0200

    gitignore

--- DoWellMarketSketch/src/com/example/dowellmarketsketch/webservice/WebService.java
+++ DoWellMarketSketch/src/com/example/dowellmarketsketch/webservice/WebService.java
@@ -8,18 +8,14 @@ import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.NameValuePair;
 import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.AuthState;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
-import org.apache.http.protocol.BasicHttpContext;
-import org.apache.http.protocol.HttpContext;
 import org.apache.http.util.EntityUtils;
 
 import android.util.Log;
